6e8f1ad88ad975243cf3b49304f60d0cb34952a5
Gani <abdulgani.work@gmail.com>
2024-11-15 11:16:27 +0530

    feat: added auth using Google

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -13,22 +13,23 @@ import {
   TabsTrigger,
 } from "@repo/ui/components/ui/tabs";
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
-import { ModeToggle } from "~/components/themes/mode-toggle";
+import { redirect } from "next/navigation";
+import { authOptions } from "~/lib/authOptions";
 import { CalendarDateRangePicker } from "./components/date-range-picker";
-import { MainNav } from "./components/main-nav";
 import { Overview } from "./components/overview";
 import { RecentSales } from "./components/recent-sales";
-import { Search } from "./components/search";
-import TeamSwitcher from "./components/team-switcher";
-import { UserNav } from "./components/user-nav";
 
 export const metadata: Metadata = {
   title: "Dashboard",
   description: "Example dashboard app built using the components.",
 };
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) redirect("/api/auth/signin?callbackUrl=/dashboard");
   return (
     <>
       <div className="md:hidden">
@@ -48,17 +49,6 @@ export default function DashboardPage() {
         />
       </div>
       <div className="hidden flex-col md:flex">
-        <AppNavBar />
-        {/* <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <TeamSwitcher />
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-              <UserNav />
-            </div>
-          </div>
-        </div> */}
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
@@ -210,21 +200,5 @@ export default function DashboardPage() {
         </div>
       </div>
     </>
-  );
-}
-
-function AppNavBar() {
-  return (
-    <div className="border-b">
-      <div className="flex h-16 items-center px-4">
-        <TeamSwitcher />
-        <MainNav className="mx-6" />
-        <div className="ml-auto flex items-center space-x-4">
-          <Search />
-          <UserNav />
-          <ModeToggle />
-        </div>
-      </div>
-    </div>
   );
 }
