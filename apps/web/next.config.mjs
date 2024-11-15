5057d56219a132859d16da8bead0f6f77b6ce0a7
Gani <abdulgani.work@gmail.com>
2024-11-15 12:04:56 +0530

    hotfix: build failure issue related to prism

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
